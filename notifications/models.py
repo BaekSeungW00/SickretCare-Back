f79b780ac8ccca52de99fa20a89e4caace0cb2dc
Seun9w00 <ph1losophe@naver.com>
2024-07-27 18:03:42 +0900

    feat: Push->Alarm 클래스 명 수정

--- notifications/models.py
+++ notifications/models.py
@@ -4,7 +4,7 @@ class Timer(models.Model):
     interval = models.IntegerField(null=True)
     user = models.OneToOneField('users.User', on_delete=models.CASCADE, related_name='timer')
 
-class Push(models.Model):
+class Alarm(models.Model):
     title = models.CharField(max_length=20)
     time = models.TimeField(auto_now=False)
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='pushes')
