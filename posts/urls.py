0f545689fabdf6342113f4a0bda391bd396797cc
ekdus04 <dygim04@gmail.com>
2024-08-01 19:17:50 +0900

    feat: like 모델 생성 / fix: like, comment view 버그 수정
    like 모델 생성
    like 모델 생성으로 인한 like 관련 view 수정
    댓글 생성, 수정, 삭제 버그 수정

--- posts/urls.py
+++ posts/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 urlpatterns = [
     path('list/', views.PostListAPIView.as_view(), name='post-list'),
-    path('get/<int:post_id>/', views.PostRetrieveDeleteAPIView.as_view(), name='post-getdelete'),
+    path('get/<int:id>/', views.PostRetrieveDeleteAPIView.as_view(), name='post-getdelete'),
     path('upload/', views.PostUploadAPIView.as_view(), name='post-upload'),
     path('liked/', views.LikedPostAPIView.as_view(), name='liked-post'),
     path('mypost/', views.MyPostAPIView.as_view(), name='my-post'),
-    path('comment/upload/', views.CommentUploadAPIView.as_view(), name='comment-upload'),
+    path('comment/list/<int:post_id>/', views.CommentListAPIView.as_view(), name='comment-list'),
+    path('comment/upload/<int:post_id>/', views.CommentUploadAPIView.as_view(), name='comment-upload'),
     path('comment/delete/<int:comment_id>/', views.CommentDeleteAPIView.as_view(), name='comment-delete'),
     path('like/<int:post_id>/', views.post_like_api_view, name='post-like'),
     path('commodity/list/', views.CommoditySearchAPIView.as_view(), name='commodity-list'),
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from rest_framework.decorators import api_view
-from posts.models import Post, Comment, Commodity, Category
-from posts.serializers import PostSerializer, CommentSerializer, CommoditySerializer, CategorySerializer
+from posts.models import Post, Comment, Commodity, Category, Like
+from posts.serializers import PostSerializer, CommentSerializer, CommoditySerializer, CategorySerializer, LikeSerializer
 from rest_framework import status
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveDestroyAPIView, DestroyAPIView
 from rest_framework.response import Response
@@ -16,7 +16,7 @@ class PostListAPIView(ListAPIView):
         # 카테고리 필터링
         category = request.GET.get('category', None)
         if category in ['치질', '변비', '과민성대장증후군']:
-            post_queryset = Post.objects.filter(category=category)
+            post_queryset = Post.objects.filter(category__name=category)
         else:
             post_queryset = Post.objects.all()
 
@@ -35,39 +35,44 @@ class PostListAPIView(ListAPIView):
 class PostRetrieveDeleteAPIView(RetrieveDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    lookup_field = 'post_id'
+    lookup_field = 'id'
 
 
 # 게시물 생성 POST
 class PostUploadAPIView(CreateAPIView):
     queryset = Post.objects.all()
-    serailizer_class = PostSerializer
+    serializer_class = PostSerializer
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    def create(self, request):
+        category_name = request.data.get("category")
+        title = request.data.get('title')
+        content = request.data.get('content')
+        category = Category.objects.get(name=category_name)
+        post = Post.objects.create(title=title, content=content, category=category, author=request.user)
+        serializer = PostSerializer(post)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 # 내가 좋아요한 게시물 전체 조회, 카테고리별 조회, 정렬 방식 선택 GET
 class LikedPostAPIView(ListAPIView):
-    serializer_class = PostSerializer
+    serializer_class = LikeSerializer
 
     def get_queryset(self):
-        return Post.objects.filter(like_users=self.request.user)
+        return Like.objects.filter(user=self.request.user)
 
     def get(self, request):
-        post_queryset = self.get_queryset()
+        like_queryset = self.get_queryset().order_by('-created_at')
+        post_queryset = []
+
+        for like in like_queryset:
+            post_queryset.append(like.post)
 
         # 카테고리 필터링
-        category = request.GET.get('category', None)
-        if category in ['치질', '변비', '과민성대장증후군']:
+        category_name = request.GET.get('category', None)
+        if category_name in ['치질', '변비', '과민성대장증후군']:
+            category = Category.objects.get(name=category_name)
             post_queryset = post_queryset.filter(category=category)
-
-        # 정렬 기준 설정
-        order_by = request.GET.get('order_by', 'created_at')
-        if order_by == '좋아요순':
-            post_queryset = post_queryset.order_by('-likes')
-        else:
-            post_queryset = post_queryset.order_by('-created_at')
 
         serializer = self.get_serializer(post_queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -78,7 +83,7 @@ class MyPostAPIView(ListAPIView):
     serializer_class = PostSerializer
 
     def get_queryset(self):
-        return Post.objects.filter(user=self.request.user)
+        return Post.objects.filter(author=self.request.user)
     
     def get(self, request):
         post_queryset = self.get_queryset()
@@ -97,6 +102,15 @@ class MyPostAPIView(ListAPIView):
 
         serializer = self.get_serializer(post_queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+# 댓글 조회 GET
+class CommentListAPIView(ListAPIView):
+    serializer_class = CommentSerializer
+    
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        return Comment.objects.filter(post__id=post_id)
 
 
 # 댓글 생성 POST
@@ -104,31 +118,40 @@ class CommentUploadAPIView(CreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
-    def perform_create(self, serializer):
-        post_id = self.request.data.get('post_id')
-        try:
-            post = Post.objects.get(id=post_id)
-        except Post.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        serializer.save(user=self.request.user, post=post)
+    def create(self, request, post_id):
+        post_id = request.data.get("post_id")
+        content = request.data.get('content')
+        post = Post.objects.get(id=post_id)
+        comment = Comment.objects.create(content=content, post=post, author=request.user)
+        serializer = CommentSerializer(comment)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 # 댓글 삭제 DELETE
 class CommentDeleteAPIView(DestroyAPIView):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     lookup_field = 'comment_id'
+
+    def get_object(self):
+        comment_id = self.kwargs.get('comment_id')
+        return Comment.objects.get(id=comment_id)
     
 
 # 좋아요 생성 및 삭제 POST
 @api_view(['POST'])
 def post_like_api_view(request, post_id):
+
     post = get_object_or_404(Post, id=post_id)
-    if post.like_users.filter(id=request.user.id).exists():
-        post.like_users.remove(request.user)
+
+    like = Like.objects.filter(post=post, user=request.user).first()
+
+    if like:
+        # 좋아요가 이미 있다면 기존 좋아요 삭제
+        like.delete()
         return Response({"공감을 취소했습니다."}, status=status.HTTP_204_NO_CONTENT)
     else:
-        post.like_users.add(request.user)
+        # 좋아요를 누르지 않았다면 새로운 좋아요를 생성
+        Like.objects.create(post=post, user=request.user)
         return Response({"공감을 눌렀습니다."}, status=status.HTTP_201_CREATED)
 
 
