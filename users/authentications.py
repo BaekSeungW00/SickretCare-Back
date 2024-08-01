26d22f6cf485ee39e23354a6651b6c2a04b45ec6
Seun9w00 <ph1losophe@naver.com>
2024-08-01 19:35:37 +0900

    feat: authentications 수정
    access token 만료 시 쿠키에서 삭제

--- users/authentications.py
+++ users/authentications.py
@@ -1,11 +1,19 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework_simplejwt.exceptions import InvalidToken
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework.response import Response
 
 class CookieBasedJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
         raw_token = request.COOKIES.get('access_token')
         if raw_token is None:
             return None
+        
+        try:
+            validated_token = self.get_validated_token(raw_token)
+        except (InvalidToken, TokenError) as e:
+            # 토큰 오류 발생 시 쿠키에서 액세스 토큰 삭제
+            response = Response(request)
+            response.delete_cookie('access_token')
+            return None
 
-        validated_token = self.get_validated_token(raw_token)
         return self.get_user(validated_token), validated_token
