6fb83b18dda596198e8d1dac24350c899f8b2486
ekdus04 <dygim04@gmail.com>
2024-08-02 23:16:54 +0900

    feat: cors 설정(5500번 포트 허용)

--- config/settings.py
+++ config/settings.py
@@ -112,6 +112,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://127.0.0.1:5500",
 ]
 
 ROOT_URLCONF = 'config.urls'
