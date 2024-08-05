e9c76e311cc4557a7bdf68211c88e024bb58bd4b
Seun9w00 <ph1losophe@naver.com>
2024-08-06 01:16:00 +0900

    feat: 회원 정보 수정 로직 수정

--- users/views.py
+++ users/views.py
@@ -71,8 +71,9 @@ def user_retrieve_update_destroy_api_view(request):
                 return Response({'error': '비밀번호가 일치하지 않음. '}, status=status.HTTP_400_BAD_REQUEST)
             else:
                 user.set_password(new_password)
-        else:
+        elif (password is None) or (new_password is None):
             return Response({'error': '현재 비밀번호와 새로운 비밀번호를 모두 입력하세요. '}, status=status.HTTP_400_BAD_REQUEST)
+            
         
         # 닉네임 변경
         if nickname is not None:
